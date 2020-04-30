76a9e231789f26914d30b80483239596413c7595
Donghyeok <tjehdgur1500@naver.com>
2020-04-30 21:59:56 +0900

    문제풀이 추가

---
+++ question/q91q100.py
@@ -0,0 +1,63 @@
+# 91 파이썬에서 True or False 를 갖는 데이터 타입은 ?
+# boolean ( bool 타입)
+
+# 92 print(3 == 5) 코드의 답은 ?
+# False
+
+# 93 print(3 < 5) 코드의 답은 ?
+# True
+
+# 94
+# x = 4
+# print(1 < x < 5)
+# Ture
+
+# 95
+# print ((3 == 3) and (4 != 3)) 코드의 답은 ?
+# True
+
+# 96
+# 아래 코드에서 에러가 발생하는 원인에 대해 설명하라.
+# print(3 => 4)
+# 비교 연산을 하는데 좌측에서 우측을 비교하는데 부등호가 우측에 있다.
+# 문법 오류
+
+# 97
+# 아래 코드의 출력 결과를 예상하라
+# if 4 < 3 :
+#    print("Hello Wrold")
+# 4 는 3보다 작지 않으므로 Flase , 아무것도 출력하지 않음.
+
+# 98
+# 아래 코드의 출력 결과를 예상하라
+# if 4 < 3:
+#     print("Hello Wrold")
+# else:
+#     print("Hi, there.")
+# else 문의 print 출력
+
+# 99
+# 아래 코드의 출력 결과를 예상하라
+# if True:
+#     print("1")
+#     print("2")
+# else:
+#     print("3")
+# print("4")
+# True는 항상 True라서 1,2 를 출력하고 4는 조건문에 안들어왔어서 출력
+
+# 100
+# 아래 코드의 출력 결과를 예상하라
+# if True:
+#     if False:
+#         print("1")
+#         print("2")
+#     else:
+#         print("3")
+# else:
+#     print("4")
+# print("5")
+# True인 조건아래 False 라는 조건이 있으므로 1,2는 항상 Fasle 다
+# 바로 나오는 else 는 다시 False 가
+# 아니라면 으로 해석할 수 있으므로 # True다
+# if 문을 충족 했으므로 else 는 실행 되지 않는다.
