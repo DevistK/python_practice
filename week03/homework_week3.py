d5f6fe3242c0f138c52f578fbc0697fcf87a6401
Donghyeok <tjehdgur1500@naver.com>
2020-10-11 20:04:39 +0900

    크롤링 음원차트까지

---
+++ week03/homework_week3.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+res = requests.get(URL, headers=headers)
+html = res.text
+
+# response object parser
+soup = BeautifulSoup(html, 'html.parser')
+
+# 음원 차트 부모요소
+chart_list = soup.select_one('#body-content > div.newest-list > div > table > tbody')
+
+# 음원차트 모든 리스트
+chart_item = chart_list.select('#body-content > div.newest-list > div > table > tbody > tr')
+print(chart_item)
